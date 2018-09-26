0099f42861e0857343360eb54fa265b3bb7e8da9
Florian Trube <flo.trube@gmail.com>
2018-09-26 14:48:08 +0200

    NumberWizard, erste Version

--- Assets/Scripts/Number.cs
+++ Assets/Scripts/Number.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class Number : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    int min;
+    int max;
+    int guess;
+
+
+    // Use this for initialization
+    void Start() {
+
+        min = 1;
+        max = 1000;
+        guess = 500;
+        Debug.Log("Hallo, gib mir bitte ein Minimum und ein Maximum! ");
+    }
+
+        // Update is called once per frame
+        void Update () {
+
+            
+
+            if (Input.GetKeyDown(KeyCode.DownArrow)) //kleiner
+            {
+                rechnen();
+                max = guess;
+                Debug.Log("max" + max);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.UpArrow)) //groesser
+            {
+                rechnen();
+                min = guess;
+                Debug.Log("min" + min);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Return)) //gleich
+            {
+                Debug.Log("Gefunden");
+            }
+
+           // Debug.Log("min" + min);
+            //Debug.Log("max" + max);
+
+            
+
+        }
+
+    void rechnen()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("BIMT DAS D1 NUMBER MOIS? " + guess);
+    }
+
 }
+
